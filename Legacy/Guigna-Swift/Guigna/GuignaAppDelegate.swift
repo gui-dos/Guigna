3d575303b2716b9bea54c8ed37c2fc82a67e97bb
Guido <gui_dos@libero.it>
2016-08-03 08:09:54 +0200

    Cast defaults["ScrapesCount"] as NSNumber

--- Legacy/Guigna-Swift/Guigna/GuignaAppDelegate.swift
+++ Legacy/Guigna-Swift/Guigna/GuignaAppDelegate.swift
@@ -1141,7 +1141,7 @@ class GuignaAppDelegate: NSObject, GAppDelegate, NSApplicationDelegate, NSMenuDe
         if self.ready && !statusField.stringValue.hasPrefix("Executing") {
             status("Scraping \(scrape.name)...")
         }
-        let scrapesCount = Double(defaults["ScrapesCount"] as! NSString as String)! // FIXME: ugly
+        let scrapesCount = Double(defaults["ScrapesCount"] as! NSNumber)
         let pagesToScrape = Int(ceil(scrapesCount / Double(scrape.itemsPerPage)))
         for i in 1...pagesToScrape {
             scrape.refresh()
